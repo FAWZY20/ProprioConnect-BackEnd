8b5a0fbcdfe8e3f16bbe78ca4d5a4ef033f6e32e
fawzy20 <fawzy-elsam@outlook.fr>
2024-12-18 20:15:06 +0100

    first commit

---
+++ src/main/java/com/ProprioConnect/ProprioConnect/ProprioConnectApplication.java
@@ -0,0 +1,13 @@
+package com.ProprioConnect.ProprioConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProprioConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProprioConnectApplication.class, args);
+	}
+
+}
